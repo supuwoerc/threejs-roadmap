0ac69a6b0ff1bfac9407339de1c6699129b2ba64
supuwoerc <zhangzhouou@gmail.com>
2024-10-31 21:49:01 +0800

    feat: 更新starter

--- 00.starter/src/main.ts
+++ 00.starter/src/main.ts
@@ -1,32 +1,30 @@
 import * as THREE from "three";
+import { OrbitControls } from "three/examples/jsm/Addons.js";
 
 const sizes = {
   width: 800,
   height: 600,
 };
-
-// 创建一个场景
 const scene = new THREE.Scene();
-
-// 创建几何形状
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-// 创建材质
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
-// 组合geometry和material
-const mesh = new THREE.Mesh(geometry, material);
-// 添加到场景中
-scene.add(mesh);
-// 创建一个相机
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-// 添加到场景
-scene.add(camera);
-// 创建渲染器
-const renderer = new THREE.WebGLRenderer({
-  canvas: document.querySelector(".webgl") as HTMLCanvasElement,
+const material = new THREE.MeshBasicMaterial({
+  color: "red",
 });
-// 设置渲染画布大小
+const mesh = new THREE.Mesh(geometry, material);
+scene.add(mesh);
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+scene.add(camera);
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector<HTMLCanvasElement>(".webgl")!,
+});
 renderer.setSize(sizes.width, sizes.height);
-// 移动相机(threejs使用的是右手坐标系)
-camera.position.z = 5;
-// 渲染
-renderer.render(scene, camera);
+const control = new OrbitControls(camera, renderer.domElement);
+control.enableDamping = true;
+control.dampingFactor = 0.01;
+const tick = () => {
+  control.update();
+  renderer.render(scene, camera);
+  window.requestAnimationFrame(tick);
+};
+tick();
